f60eedb547193ae4ce7f4b3a84341cf89b525121
Kamal <kamalpat@buffalo.edu>
2021-09-18 23:44:32 -0400

    Added the toggle read button functionality if Yes and No

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 counter = 0;
 index = 0;
+index2 = 0;
 let booksArray = []
 
 function Book(title, author, numberOfPages, hasBeenRead) {
@@ -35,6 +36,7 @@ function cards() {
     const content3 = document.createElement('p');
     content3.textContent = `Number Of Pages: ${booksArray[index].numberOfPages}`
     const content4 = document.createElement('p');
+    content4.setAttribute('id', index2);
     content4.textContent = `Has Been Read: ${booksArray[index].hasBeenRead}`
     
     const btn = document.createElement('button');
@@ -47,14 +49,29 @@ function cards() {
         innerContainers.remove();
     };
 
+    const btn2 = document.createElement('button');
+    btn2.innerHTML = 'Toggle Read Book';
+    btn2.name = 'toggle';
+    btn2.classList.add('togglebtn');
+    btn2.setAttribute('id', 'togglebtn');
+    btn2.onclick = function() {
+        if (content4.textContent == 'Has Been Read: Yes') {
+            content4.textContent = 'Has Been Read: No';
+        } else if (content4.textContent == 'Has Been Read: No') {
+            content4.textContent = 'Has Been Read: Yes';
+        }
+    };
+
 
     innerContainer.appendChild(content);
     innerContainer.appendChild(content2);
     innerContainer.appendChild(content3);
     innerContainer.appendChild(content4);
     innerContainer.appendChild(btn);
+    innerContainer.appendChild(btn2);
     container.appendChild(innerContainer);
     index++;
+    index2++;
     console.log(innerContainer.id); //this line confirms each card has it's own id.
     //you should be able to add     buttons that know their id and card that they are
     //supposed to delete. Not sure how to delete them though? innetHTML ''?
