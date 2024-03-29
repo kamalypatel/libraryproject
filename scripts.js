1fe69a87f82b8860ac78c1956b399b69f1ff9517
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-29 17:46:42 -0400

    started adding the modal and working on the javascript functions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const myLibrary = []
-
-function Book(title, author) {
+let i = 0
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary(array){
@@ -10,9 +12,9 @@ function addBookToLibrary(array){
 }
 
 
-const teachYouToBeRich = new Book('I will teach you to be rich', 'Ramit Sethi')
+const teachYouToBeRich = new Book('I will teach you to be rich', 'Ramit Sethi', 200, 'yes')
 
-const harryPotter = new Book('Harry Potter', 'JK Rowling')
+const harryPotter = new Book('Harry Potter', 'JK Rowling', 200, 'yes')
 
 addBookToLibrary(teachYouToBeRich)
 
@@ -20,14 +22,29 @@ addBookToLibrary(harryPotter)
 
 console.log(myLibrary)
 
-function addBookToLibrary(){
+function addBookToLibrary(nextBook){
+    myLibrary.push(nextBook)
     const table = document.querySelector('table')
-    const currentBook = document.createElement(tr)
-    const currentBookTitle = document.createElement(td)
-    currentBookTitle.innerHTML = title
-    const currentBookAuthor = document.createElement(td)
-    currentBookAuthor.innerHTML = author
+    const currentBook = document.createElement('tr')
+    const currentBookTitle = document.createElement('td')
+    currentBookTitle.innerHTML = nextBook.title
+    const currentBookAuthor = document.createElement('td')
+    currentBookAuthor.innerHTML = nextBook.author
     currentBook.appendChild(currentBookTitle)
     currentBook.appendChild(currentBookAuthor)
     table.appendChild(currentBook)
 }
+
+const button = document.getElementById('addBook')
+
+/* saveButton.addEventListener('click', (title, author, pager, read) => {
+    const nextBook = new Book(title, author, pages, read)
+    console.log(nextBook.title)
+    addBookToLibrary(nextBook)
+}) */
+
+const modal = document.querySelector('dialog')
+
+button.addEventListener('click', () => {
+    modal.showModal();
+})
